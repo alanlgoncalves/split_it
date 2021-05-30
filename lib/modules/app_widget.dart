092676697596bee3574c268978aed15b0da2eca8
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-05-30 19:30:09 -0300

    Add page routes to the App

--- lib/modules/app_widget.dart
+++ lib/modules/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:split_it/modules/error/error_page.dart';
+import 'package:split_it/modules/login/login_page.dart';
 import 'package:split_it/modules/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -6,7 +8,12 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Split.it",
-      home: SplashPage(),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/login": (context) => LoginPage(),
+        "/erro": (context) => ErrorPage(),
+      },
     );
   }
 }
