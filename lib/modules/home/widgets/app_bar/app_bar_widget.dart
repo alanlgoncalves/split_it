5a300ce84716447d986c25baa8f7e373d417147f
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-06-20 18:28:42 -0300

    feat: Dashboard bottom app bar stateful widget

--- lib/modules/home/widgets/app_bar/app_bar_widget.dart
+++ lib/modules/home/widgets/app_bar/app_bar_widget.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/home/widgets/add_button_widget.dart';
+import 'package:split_it/modules/home/widgets/bottom_app_bar/bottom_app_bar_widget.dart';
 import 'package:split_it/modules/login/models/user_model.dart';
 import 'package:split_it/theme/app_theme.dart';
-
-import 'info_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   final UserModel user;
@@ -43,17 +42,7 @@ class AppBarWidget extends PreferredSize {
                     SizedBox(
                       height: 36,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        InfoCardWidget(
-                          value: 100,
-                        ),
-                        InfoCardWidget(
-                          value: -48,
-                        )
-                      ],
-                    )
+                    BottomAppBarWidget(),
                   ],
                 ),
               ],
