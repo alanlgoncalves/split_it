1ccdbc9fed4d8ce6fd01d4b9414eb0985c433b9e
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-06-20 14:36:27 -0300

    fix: File name extension

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/home/widgets/app_bar_widget.dart';
-import 'package:split_it/modules/home/widgets/event_tile_widget.dar.dart';
+import 'package:split_it/modules/home/widgets/event_tile_widget.dart';
 import 'package:split_it/modules/login/models/user_model.dart';
 
 class HomePage extends StatefulWidget {
