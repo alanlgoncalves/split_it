9df6b9d98e0a1eacdbd55f8f81ffb0d17c98ea41
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-07-27 00:37:05 -0300

    feat: Use SliverAppBar on home page

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/home/home_controller.dart';
-import 'package:split_it/modules/home/home_state.dart';
 import 'package:split_it/modules/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:split_it/modules/home/widgets/bottom_app_bar/bottom_app_bar_widget.dart';
 import 'package:split_it/modules/home/widgets/event_tile_widget.dart';
 import 'package:split_it/modules/login/models/user_model.dart';
 import 'package:split_it/shared/models/event_model.dart';
+import 'package:split_it/theme/app_theme.dart';
+
+import 'home_state.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -29,37 +32,75 @@ class _HomePageState extends State<HomePage> {
         ModalRoute.of(context)!.settings.arguments as UserModel;
 
     return Scaffold(
-      appBar: AppBarWidget(
-        user: user,
-        onTapAddButton: () {
-          Navigator.pushNamed(context, "/create_split");
-        },
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              if (controller.state is HomeStateLoading) ...[
-                ...List.generate(
-                    4,
-                    (index) =>
-                        EventTileWidget(event: EventModel(), isLoading: true))
-              ] else if (controller.state is HomeStateSuccess) ...[
-                ...(controller.state as HomeStateSuccess)
-                    .events
-                    .map((event) => EventTileWidget(
-                          event: event,
-                          isLoading: false,
-                        ))
-              ] else if (controller.state is HomeStateFailure) ...[
-                Text((controller.state as HomeStateFailure).message)
-              ] else ...[
-                Container()
-              ]
-            ],
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 80,
+            collapsedHeight: 80,
+            elevation: 0,
+            flexibleSpace: AppBarWidget(
+              user: user,
+              onTapAddButton: () {
+                Navigator.pushNamed(context, "/create_split");
+              },
+            ),
           ),
-        ),
+          SliverAppBar(
+            expandedHeight: 170,
+            collapsedHeight: 80,
+            backgroundColor: Colors.transparent,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Column(
+                children: [
+                  Container(
+                    height: 26,
+                    color: AppTheme.colors.backgroundSecondary,
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        color: AppTheme.colors.backgroundSecondary,
+                        height: 80,
+                      ),
+                      BottomAppBarWidget(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      if (controller.state is HomeStateLoading) ...[
+                        ...List.generate(
+                            4,
+                            (index) => EventTileWidget(
+                                event: EventModel(), isLoading: true))
+                      ] else if (controller.state is HomeStateSuccess) ...[
+                        ...(controller.state as HomeStateSuccess)
+                            .events
+                            .map((event) => EventTileWidget(
+                                  event: event,
+                                  isLoading: false,
+                                ))
+                      ] else if (controller.state is HomeStateFailure) ...[
+                        Text((controller.state as HomeStateFailure).message)
+                      ] else ...[
+                        Container()
+                      ]
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
--- lib/modules/home/widgets/app_bar/app_bar_widget.dart
+++ lib/modules/home/widgets/app_bar/app_bar_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/home/widgets/add_button_widget.dart';
-import 'package:split_it/modules/home/widgets/bottom_app_bar/bottom_app_bar_widget.dart';
 import 'package:split_it/modules/login/models/user_model.dart';
 import 'package:split_it/theme/app_theme.dart';
 
@@ -14,38 +13,32 @@ class AppBarWidget extends PreferredSize {
               children: [
                 Container(
                   color: AppTheme.colors.backgroundSecondary,
-                  height: 240,
-                ),
-                Column(
-                  children: [
-                    Container(
-                      color: AppTheme.colors.backgroundSecondary,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 62),
-                        child: ListTile(
-                          leading: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              user.photoUrl!,
+                  child: SafeArea(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 14),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                user.photoUrl!,
+                              ),
+                            ),
+                            title: Text(
+                              user.name!,
+                              style: AppTheme.textStyles.appBar,
+                            ),
+                            trailing: AddButtonWidget(
+                              onTap: onTapAddButton,
                             ),
                           ),
-                          title: Text(
-                            user.name!,
-                            style: AppTheme.textStyles.appBar,
-                          ),
-                          trailing: AddButtonWidget(
-                            onTap: onTapAddButton,
-                          ),
                         ),
-                      ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 36,
-                    ),
-                    BottomAppBarWidget(),
-                  ],
+                  ),
                 ),
               ],
             ),
-            preferredSize: Size.fromHeight(298));
+            preferredSize: Size.fromHeight(80));
 }
