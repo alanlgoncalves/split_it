3087d7ac5bf147a50db9f6e737e814f77bdbb9f4
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-07-29 17:20:26 -0300

    fix: App bar controller tests

--- test/app_bar_controller.dart
+++ test/app_bar_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobx/mobx.dart' as mobx;
 import 'package:mocktail/mocktail.dart';
 import 'package:split_it/modules/home/models/dashboard_model.dart';
 import 'package:split_it/modules/home/repositories/home_repository.dart';
@@ -20,29 +21,36 @@ void main() {
     expect(controller.state, isInstanceOf<AppBarStateEmpty>());
 
     final states = <AppBarState>[];
-    controller.listen((state) => states.add(state));
+
+    mobx.autorun((_) {
+      states.add(controller.state);
+    });
 
     when(repository.getDashboard)
         .thenAnswer((_) async => DashboardModel(receive: 100, send: 101));
 
     await controller.getDashboard();
 
-    expect(states[0], isInstanceOf<AppBarStateLoading>());
-    expect(states[1], isInstanceOf<AppBarStateSuccess>());
-    expect(states.length, 2);
+    expect(states[0], isInstanceOf<AppBarStateEmpty>());
+    expect(states[1], isInstanceOf<AppBarStateLoading>());
+    expect(states[2], isInstanceOf<AppBarStateSuccess>());
+    expect(states.length, 3);
   });
 
   test("Testing getDashboard with Failure", () async {
     expect(controller.state, isInstanceOf<AppBarStateEmpty>());
 
     final states = <AppBarState>[];
-    controller.listen((state) => states.add(state));
+
+    mobx.autorun((_) {
+      states.add(controller.state);
+    });
 
     when(repository.getDashboard).thenThrow("Error");
 
     await controller.getDashboard();
 
-    expect(states[0], isInstanceOf<AppBarStateLoading>());
+    expect(states[0], isInstanceOf<AppBarStateEmpty>());
     expect(states[1], isInstanceOf<AppBarStateFailure>());
     expect((states[1] as AppBarStateFailure).message, "Error");
     expect(states.length, 2);
