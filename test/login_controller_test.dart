87fc22804e0d62f3bb9bece7a51314bb9d7e71bb
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-07-29 17:13:51 -0300

    fix: Login controller tests

--- test/login_controller_test.dart
+++ test/login_controller_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobx/mobx.dart' as mobx;
 import 'package:mocktail/mocktail.dart';
 import 'package:split_it/modules/login/login_controller.dart';
 import 'package:split_it/modules/login/login_service.dart';
@@ -8,50 +9,56 @@ import 'package:split_it/modules/login/models/user_model.dart';
 class LoginServiceMock extends Mock implements LoginService {}
 
 void main() {
-  late LoginControllerBase controller;
+  late LoginController controller;
   late LoginService service;
 
   setUp(() {
     service = LoginServiceMock();
-    controller = LoginControllerBase(service: service, onUpdate: () {});
+    controller = LoginController(service: service);
   });
 
   test("Testing Google sign in return success", () async {
     expect(controller.state, isInstanceOf<LoginStateEmpty>());
 
     final states = <LoginState>[];
-    controller.listen((state) => states.add(state));
+
+    mobx.autorun((state) {
+      states.add(controller.state);
+    });
 
     when(service.googleSignIn).thenAnswer(
         (_) async => UserModel(id: "1", email: "john.doe@gmail.com"));
 
     await controller.googleSignIn();
 
-    expect(states[0], isInstanceOf<LoginStateLoading>());
-    expect(states[1], isInstanceOf<LoginStateSuccess>());
-    expect(states.length, 2);
+    expect(states[0], isInstanceOf<LoginStateEmpty>());
+    expect(states[1], isInstanceOf<LoginStateLoading>());
+    expect(states[2], isInstanceOf<LoginStateSuccess>());
+    expect(states.length, 3);
   });
 
   test("Testing Google sign in return failure", () async {
     expect(controller.state, isInstanceOf<LoginStateEmpty>());
 
     final states = <LoginState>[];
-    controller.listen((state) => states.add(state));
+    mobx.autorun((state) {
+      states.add(controller.state);
+    });
 
     when(service.googleSignIn).thenThrow("Error");
 
     await controller.googleSignIn();
 
-    expect(states[0], isInstanceOf<LoginStateLoading>());
+    expect(states[0], isInstanceOf<LoginStateEmpty>());
     expect(states[1], isInstanceOf<LoginStateFailure>());
     expect((states[1] as LoginStateFailure).message, "Error");
     expect(states.length, 2);
   });
 
-  test("Testing update method", () async {
+  test("Testing listen method", () async {
     controller.state = LoginStateLoading();
-    controller
-        .listen((state) => expect(state, isInstanceOf<LoginStateLoading>()));
-    controller.update();
+
+    mobx.autorun(
+        (_) => {expect(controller.state, isInstanceOf<LoginStateLoading>())});
   });
 }
